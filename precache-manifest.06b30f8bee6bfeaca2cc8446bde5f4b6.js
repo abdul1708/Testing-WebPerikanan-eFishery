656f09aaff1d9c7ef57a1f61680fb1b42d881258
rian-gustandi <rian.gustandi17@gmail.com>
2020-06-24 10:50:15 +0700

    Updates

---
+++ precache-manifest.06b30f8bee6bfeaca2cc8446bde5f4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4c58e6729a5653fd22dd947514c281f",
+    "url": "/Testing-WebDataPerikanan-eFishery/index.html"
+  },
+  {
+    "revision": "c277ddb96cdaa0f31c45",
+    "url": "/Testing-WebDataPerikanan-eFishery/static/css/main.67ee3c64.chunk.css"
+  },
+  {
+    "revision": "947f18f40aab9d40bb37",
+    "url": "/Testing-WebDataPerikanan-eFishery/static/js/2.22d024e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Testing-WebDataPerikanan-eFishery/static/js/2.22d024e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c277ddb96cdaa0f31c45",
+    "url": "/Testing-WebDataPerikanan-eFishery/static/js/main.420ff2a1.chunk.js"
+  },
+  {
+    "revision": "74c6d0d5301aec42b2a2",
+    "url": "/Testing-WebDataPerikanan-eFishery/static/js/runtime-main.af7cedd9.js"
+  }
+]);
